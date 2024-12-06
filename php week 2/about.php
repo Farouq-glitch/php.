557638949eb9898cc988b67ae5273d75448237d2
Farouq-glitch <9024772@student.zadkine.nl>
2024-12-06 13:57:05 +0100

    .

--- php week 2/about.php
+++ php week 2/about.php
@@ -7,5 +7,7 @@
 </head>
 <body>
 <?php include 'navbar.php'?>
+
+
 </body>
 </html>
--- php week 2/contact.php
+++ php week 2/contact.php
@@ -1,1 +1,24 @@
 <?php include 'navbar.php'?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php include 'navbar.php'?>
+<label for="naam"></label>
+<input type="type" id="fname"  name='name'>
+<br>
+
+<label for="bericht"></label>
+<input type="type" id="fname"  name='message'>
+<br>
+
+<label for="E-mail"></label>
+<input type="type" id="fname"  name='E-mail'> <br> <br>
+<input type="submit" value="Submit">     >
+<br>
+</body>
+</html>
